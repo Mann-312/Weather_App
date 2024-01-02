45478021f6408656795c4c051b65f16d9c96c6e4
Mann-312 <132891224+Mann-312@users.noreply.github.com>
2024-01-02 14:21:16 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "bb9dc4ca8f5bce96f89fa9831dbb439d",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
